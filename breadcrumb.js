a1af0a2388e77db1217c62039e4e23a9ed8fc4b8
TS15825868 <tung314069@gmail.com>
2026-03-15 23:21:18 +0800

    Update breadcrumb.js

--- breadcrumb.js
+++ breadcrumb.js
@@ -1,6 +1,10 @@
+document.addEventListener("DOMContentLoaded",()=>{
+
 const title=document.querySelector("h1");
 const bc=document.getElementById("breadcrumb-title");
 
 if(title && bc){
-bc.textContent=title.textContent;
+bc.textContent=title.textContent.trim();
 }
+
+});
