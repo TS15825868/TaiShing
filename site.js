6b0f060c26ac025b009a96c6a7ad1b1904c5110b
TS15825868 <tung314069@gmail.com>
2026-03-16 13:03:36 +0800

    Add files via upload

--- site.js
+++ site.js
@@ -10,17 +10,12 @@ const base=isArticle?"../":"";
 
 menu.innerHTML=`
 <a href="${base}index.html">首頁</a>
-<a href="${base}guilu-series.html">龜鹿系列</a>
-<a href="${base}recipes.html">料理搭配</a>
-<a href="${base}choose.html">怎麼選龜鹿</a>
 <a href="${base}articles.html">龜鹿知識</a>
-<a href="${base}brand.html">品牌故事</a>
-<a href="${base}faq.html">FAQ</a>
 <a href="https://lin.ee/sHZW7NkR" target="_blank">LINE詢問</a>
 `;
 }
 
-if(btn && menu){
+if(btn&&menu){
 btn.addEventListener("click",()=>menu.classList.toggle("active"));
 menu.querySelectorAll("a").forEach(a=>a.addEventListener("click",()=>menu.classList.remove("active")));
 }
